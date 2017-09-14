69c0f90fab05221b68a06e86acd2f734d6a9bd52
innkeeper77 <30507952+innkeeper77@users.noreply.github.com>
2017-09-13 22:58:07 -0600

    Create cards.sh
    Initial commit of program. Currently in a non-working state

---
+++ cards.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# cards.sh: command line flashcard study tool
+
+# Adding in argument count processing code. Currently I am just seeing what works
+echo "Positional Parameters"
+echo '$0 = ' $0
+echo '$1 = ' $1
+echo '$2 = ' $2
+echo '$3 = ' $3
