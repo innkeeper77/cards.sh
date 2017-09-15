09774b5e2b1f28b48057a1741f5400224afd11a5
Erik <erik.innkeeper77@gmail.com>
2017-09-15 13:05:02 -0600

    Completed coding for all remaining functions. Can now modify flashcard sets

--- cards.sh
+++ cards.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
 # cards.sh: command line flashcard study tool
 
-# Adding in argument count processing code. Currently I am just seeing what works
-
-# First, check for arguments. If none, print help and exit
-
-main () { # Main code is wrapped, and called at bottom of script. 
-	#This is so functions can be declared at the bottom of the code instead of the top, 
-	#for improved readability
+main () { 
+	# For readability main code is wrapped as a function, and called at bottom of script. 
+	# This is so functions can be declared at the bottom of the code instead of the top 
 	
-selectedset=$2
+selectedset=$2 # Second command line argument, saved as a variable for simplicity
 
 if [ $# -eq 0 ]; then
-  printf -- "No arguments. Use -h for help\n"
+  printf -- "No arguments. Use -h for help\n" # Stops script if no command line arguments provided
   
 elif [ "$opt" = "?" ]
 then
   echo "Default option executed (by default)"
 else # using getopts to parse command line options
-while getopts "l:a:n:w:r:d:hs" opt; do #lanwrd - all take one argument
+while getopts "l:a:n:w:r:d:hs" opt; do 
 	case ${opt} in
-		l ) #learned mode playback
+		l ) 			# All modes simply call the appropriate fuctions, declared below
 			LEARNED
 			;;
 		a )
-			ALL #all mode playback- ignore learned
+			ALL 
 			;;
-		n ) # New flashcard set
+		n ) 
 			NEWSET
 			;;
-		w ) # Write flashcards to existing set
+		w ) 
 			WRITETO
 			;;
-		r ) # reset learned list
+		r ) 
 			RESETLEARNED
 			;;
-		d ) # delete flashcard set and learned list
+		d ) 
 			DELETESET
 			;;
-		h ) #show help
+		h ) 
 			HELP
 			;;
-		s ) #process -s (list)
+		s ) 
 			LIST
 			;;
 		\? ) printf -- "Incorrect argument. Use -h for help\n"
@@ -51,7 +47,6 @@ while getopts "l:a:n:w:r:d:hs" opt; do #lanwrd - all take one argument
 fi
 }
 
-#Help function
 HELP () {
 	printf "Usage:\n"
 	printf "cards.sh <options> <flashcard set>\n\n"
@@ -67,8 +62,9 @@ HELP () {
 
 #TODO: Write list function that is just a ls of dirs in the directory
 LIST () {	
-	printf "Code not yet implemented. Todo: incorporate list function\n"
-}
+	ls sets/*.cards
+	exit 0
+} 
 
 LEARNED () {	
 	printf "Problem set selected: %s  --  Ignore learned flashcardscards mode\n" $selectedset
@@ -77,16 +73,16 @@ LEARNED () {
 		printf "Flashcard set does not exist\n"
 		exit 2
 	else
-		printf "Flashcard set exists!\n\n"
+		# printf "Flashcard set exists!\n\n"
 		while true
 		do
 			echo #newline
-			# Simple check for empty flashcard set
-			if [ ! -s sets/${selectedset}.cards ]; then
-			printf "Flashcard set appears to be empty\n"
+			if [ ! -s sets/${selectedset}.cards ]; then # Simple check for empty flashcard set
+			printf "Unlearned flashcard set is empty\n"
 			exit 2
 			fi
 			
+			# Main flashcard playback loop
 			shuf -n 1 sets/${selectedset}.cards > .temp.cards # writes a random question to a temp file in the main directory
 			printf "\nQuestion: "
 			awk -F'======2======' '{print $1}' .temp.cards # Print question from temp file
@@ -142,12 +138,14 @@ ALL () {
 		printf "Flashcard set does not exist\n"
 		exit 2
 	else
-		printf "Flashcard set exists!\n\n"
-		# Simple check for empty flashcard set
-		if [ ! -s sets/${selectedset}.cards ] && [ ! -s sets/${selectedset}.cards.learned ]; then
+		# printf "Flashcard set exists!\n\n"
+		
+		if [ ! -s sets/${selectedset}.cards ] && [ ! -s sets/${selectedset}.cards.learned ]; then # Simple check for empty flashcard set. Since this is the "all" mode, we techinically only need one to be not empty
 		printf "Flashcard set appears to be empty\n"
 		exit 2
 		fi
+		
+		# Main flashcard playback loop
 		while true
 		do
 			if (( $RANDOM % 2 == 0 )); 
@@ -258,37 +256,103 @@ ALL () {
 }
 
 NEWSET () {	
-	printf "Code not yet implemented. Todo: incorporate new set function\n"
-	## When done, prompt to go to card creation in new set
+	if [ -e sets/"${selectedset}.cards" ]; then # -d checks for directory of variable name. ! means code will run if false. Else, problem set directory must exist, and code continues
+		printf "Flashcard set already exists\n"
+		exit 2
+	fi
+	:> sets/"$selectedset".cards
+	:> sets/"$selectedset".cards.learned
+	printf "Empty .cards and .cards.learned files created\n"
+	printf "Would you like to write cards in this new set? "
+	while true; do
+				read -p "[Y|n] " choice
+				case "$choice" in
+					y|Y )
+					WRITETO
+					;;
+					n|N  )
+						printf "exit\n"
+						exit 0
+					;;
+					* ) 
+					printf "Assuming yes\n"
+					WRITETO
+					;;
+				esac
+			done
 }
 
+
 WRITETO () {	
-	printf "Code not yet implemented. Todo: incorporate write cards function\n"
+	
+	while true; do
+	printf "Write a new flashcard to %s?" selectedset
+	read -p " [Y|n] " choice
+				case "$choice" in
+					y|Y )
+					echo -n "Question:"
+					read NQES # new question variable
+					echo -n "Answer:"
+					read NWAN # new answer variable
+					echo -n "Hint (if any):"
+					read NHIN # new hint variable
+					echo "$NQES======2======$NWAN======2======$NHIN" >> sets/${selectedset}.cards #appends to bottom of unlearned list
+					;;	
+					
+					n|N  )
+						printf "exit\n"
+						exit 0
+					;;
+					* ) 
+					printf "Assuming yes\n"
+					echo -n "Question:"
+					read NQES # new question variable
+					echo -n "Answer:"
+					read NWAN # new answer variable
+					echo -n "Hint (if any):"
+					read NHIN # new hint variable
+					echo "$NQES======2======$NWAN======2======$NHIN" >> sets/${selectedset}.cards #appends to bottom of unlearned list
+					;;
+				esac
+		done
 }
 
 RESETLEARNED () {	
-	printf "Code not yet implemented. Todo: create function to reset learned list\n"
+	if [ ! -e sets/"${selectedset}.cards" ]; then # -d checks for directory of variable name. ! means code will run if false. Else, problem set directory must exist, and code continues
+		printf "Flashcard set does not exist\n"
+		exit 2
+	fi
+	cat sets/${selectedset}.cards.learned >> sets/${selectedset}.cards # Append contents of .cards.learned file to bottom of .cards file
+	:> sets/${selectedset}.cards.learned # replace .cards.learned file with empty file
+	printf "All learned cards moved to unlearned set\nexit\n"
+	exit 0
 }
 
 DELETESET () {	
-	printf "Code not yet implemented. Todo: create function to delete a set and related files\n"
+	if [ ! -e sets/"${selectedset}.cards" ]; then # -d checks for directory of variable name. ! means code will run if false. Else, problem set directory must exist, and code continues
+		printf "Flashcard set does not exist\n"
+		exit 2
+	fi
+	rm -f sets/${selectedset}.cards.learned
+	rm -f sets/${selectedset}.cards
+	printf "Flashcard set deleted\nexit\n"
+	exit 0
 }
 
-CRCT () {
+CRCT () { # function to move card from the unlearned list > learned list
 	cat .temp.cards >> sets/${selectedset}.cards.learned
-	#NOT WORKING - could be faster if fixed - awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 .temp.cards f=2 sets/${selectedset}.cards
 	grep -Fvx -f .temp.cards sets/${selectedset}.cards > remaining.list #Writes new file without correct car in it
 	mv remaining.list sets/${selectedset}.cards #replaces cards list with the new file, missing the correct card, which is now in the .learned file
 	printf "Moved card to learned list\n\n"
 }
 
-WRNG () {
+WRNG () { # function to move card from the learned list > unlearned list (if forgotten. Only used by review all mode)
 	cat .temp.cards >> sets/${selectedset}.cards
 	grep -Fvx -f .temp.cards sets/${selectedset}.cards.learned > remaining.list #Writes new file without incorrect card in it
 	mv remaining.list sets/${selectedset}.cards.learned #replaces cards list with the new file, missing the incorrect card, which is now in the main unlearned file
 	printf "Moved card to unlearned list\n\n"
 }
 
-main "$@" #calls main code from top, only after all functions have been processed (readability concerns)
+main "$@" #calls main code from top, only after all functions have been processed (readability concerns) - bash does not allow forward declarations
 
 
